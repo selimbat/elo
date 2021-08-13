af41331c89bfad8936a2d32b10f49950b3fc9085
selimbat <selim.tirellil@student.ecp.fr>
2021-08-13 18:50:16 +0200

    create a first get endpoint

--- api/app.js
+++ api/app.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-})
+app.use('/api/candidates', (req, res, next) => {
+  const candidates = [
+    {
+      id: '1',
+      firstname: "Michel",
+      lastname: "Dupont",
+      party: "parti A",
+      score: 1200
+    },
+    {
+      id: '1',
+      firstname: "Martine",
+      lastname: "Dubois",
+      party: "parti B",
+      score: -2105
+    },
+  ];
+  res.status(200).json(candidates);
+});
 
 module.exports = app;
