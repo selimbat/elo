fe6236cdaa38350330415dd4ec78b450e26898af
selimbat <selim.tirellil@student.ecp.fr>
2021-09-04 00:14:14 +0200

    Change outcome values from int to enum

--- api/resources/Encounter.js
+++ api/resources/Encounter.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
+const Outcome = {
+  MORE_LEFT: 1,
+  SIMILAR: 0,
+  MORE_RIGHT: -1
+}
+
 const encounterSchema = mongoose.Schema({
   candidate1Id: { type: mongoose.ObjectId, required: true },
   candidate2Id: { type: mongoose.ObjectId, required: true },
@@ -12,7 +18,7 @@ encounterSchema.statics.checkOutcomeValidity = function(outcome) {
   // 1 if candidate1 is judged more left-leaning than candidate2
   // 0 if candidate1 is judged similar to candidate2
   // -1 if candidate1 is judged more right-leaning than candidate2
-  if (outcome !== 1 && outcome !== 0 && outcome !== -1){
+  if (outcome !== Outcome.MORE_LEFT && outcome !== Outcome.SIMILAR && outcome !== Outcome.MORE_RIGHT) {
     throw new Error(`Unsupported outcome value (${outcome}) for Encounter.`);
   }
 }
