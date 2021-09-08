35c45eb8d8fa24384801685911fe6c775f273c8e
selimbat <selim.tirellil@student.ecp.fr>
2021-09-08 20:46:33 +0200

    Add a new endpoint that gives two random candidates

--- api/app.js
+++ api/app.js
@@ -30,7 +30,27 @@ app.get('/api/candidates', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
-app.get('/api/candidate/:id', (req, res, next) => {
+app.get('/api/candidates/random-two', (req, res, next) => {
+  try {
+    Candidate.countDocuments(async (err, count) => {
+      if (err) {
+        throw err;
+      }
+      let rand = Math.floor(Math.random() * count);
+      let candidate1 = await Candidate.findOne().skip(rand);
+      let candidate2;
+      do {
+        rand = Math.floor(Math.random() * count);
+        candidate2 = await Candidate.findOne().skip(rand);
+      } while (candidate2._id == candidate1._id);
+      res.status(200).json([candidate1, candidate2]);
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+})
+
+app.get('/api/candidates/:id', (req, res, next) => {
   Candidate.findOne({ _id: req.params.id })
     .then(candidate => res.status(200).json(candidate))
     .catch(error => res.status(404).json({ error }));
@@ -66,7 +86,6 @@ app.post('/api/encounter', async (req, res, next) => {
       message: `Encounter between candidates of id ${encounterResult.items[0].candidateId} and ${encounterResult.items[1].candidateId} succeffully registered. The winner gained ${Math.abs(encounterResult.items[0].scoreDiff)}`
     });
   } catch (error) {
-    console.log(error);
     res.status(400).json(error);
   }
 });
