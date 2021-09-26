a15af98c1c1fd7ce9639ee61562e17a54b1ee507
selimbat <selim.tirellil@student.ecp.fr>
2021-09-26 18:37:56 +0200

    Add db name to env variables

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ const encounterRouter = require('./routers/Encounter');
 
 const app = express();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.mlx2e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.mlx2e.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
 {
   useNewUrlParser: true,
   useUnifiedTopology: true
