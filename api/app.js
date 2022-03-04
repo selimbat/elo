251fcbf8cb372f8ce0192b93e2b61f63cd262e23
selimbat <selim.tirellil@student.ecp.fr>
2022-03-04 22:49:52 +0100

    Add csp header to all api requests

--- api/app.js
+++ api/app.js
@@ -26,6 +26,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Content-Security-Policy', 'default-src \'self\'');
   next();
 });
 
