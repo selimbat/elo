8e311e2a401ea5000c656ef99db9bb21dd37ee2a
selimbat <selim.tirellil@student.ecp.fr>
2022-03-05 00:13:04 +0100

    try to fix heroku static files

--- api/app.js
+++ api/app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/public", express.static("public"));
 
 app.use("/api/candidates", candidateRouter);
 app.use("/api/encounter", encounterRouter);
